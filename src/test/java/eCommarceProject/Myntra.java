0f73f6b14793927ac538e590cc01c623c26beed1
SwapnilDeore746 <swapdeore47@gmail.com>
2023-07-30 20:37:50 +0530

    [SwapnilDeore746] add new test case Myntra

---
+++ src/test/java/eCommarceProject/Myntra.java
@@ -0,0 +1,19 @@
+package eCommarceProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Myntra {
+  @Test
+  public void myntratest() throws InterruptedException 
+  {
+	  WebDriver driver = new ChromeDriver();
+	  Reporter.log("opening myntra", true);
+	  driver.get("https://www.myntra.com/");
+	  Thread.sleep(7000);
+	  driver.close(); 
+  }
+  }
+
